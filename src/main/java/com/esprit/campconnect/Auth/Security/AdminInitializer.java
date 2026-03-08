f5bdc17b777d0a16e772777c75723009f35447ea
chaymaSm <samaalichayma99@gmail.com>
2026-03-08 16:17:13 +0100

    Add email for admin

---
+++ src/main/java/com/esprit/campconnect/Auth/Security/AdminInitializer.java
@@ -0,0 +1,33 @@
+package com.esprit.campconnect.Auth.Security;
+
+import com.esprit.campconnect.User.Entity.Role;
+import com.esprit.campconnect.User.Entity.Utilisateur;
+import com.esprit.campconnect.User.Repository.UtilisateurRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+
+    private final UtilisateurRepository utilisateurRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+
+        if(utilisateurRepository.findByEmail("admin@campconnect.com").isEmpty()) {
+
+            Utilisateur admin = new Utilisateur();
+
+            admin.setNom("Admin");
+            admin.setEmail("admin@campconnect.com");
+            admin.setMotDePasse(passwordEncoder.encode("admin123"));
+            admin.setRole(Role.ADMINISTRATEUR);
+
+            utilisateurRepository.save(admin);
+        }
+    }
+}
