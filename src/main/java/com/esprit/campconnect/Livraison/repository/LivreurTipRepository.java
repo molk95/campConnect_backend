0365cfb99ac676325a11c5e72f108b975a9c3802
molk95 <molk.saouabi@gmail.com>
2026-04-29 21:46:13 +0100

    feat: fix Stripe tip flow

---
+++ src/main/java/com/esprit/campconnect/Livraison/repository/LivreurTipRepository.java
@@ -0,0 +1,4 @@
+package com.esprit.campconnect.Livraison.repository;
+
+public interface LivreurTipRepository {
+}
---
+++ src/main/java/com/esprit/campconnect/common/LivraisonEmailTemplateService.java
@@ -0,0 +1,4 @@
+package com.esprit.campconnect.common;
+
+public class LivraisonEmailTemplateService {
+}
